7807449aac14de8e58e08747d63a18a8492d1e7c
ed lane <ed.lane.0@gmail.com>
2015-02-08 22:10:22 -0700

    changes to config

--- rerun/copy_to_test.sh
+++ rerun/copy_to_test.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -v
 
 cd ~/dev/salt-reduce/rerun
+
 mkdir -p /srv/salt/_runners/lib
 touch /srv/salt/_runners/lib/__init__.py
 mkdir -p /srv/salt/_returners/
 mkdir -p /srv/salt/_modules/
 
+cp -uv conf/master.conf /etc/salt/master.d/
 cp -uv rerun_runner.py /srv/salt/_runners
 cp -uv mapper.py /srv/salt/_runners/lib
 cp -uv rerun_return.py /srv/salt/_returners
 cp -uv mapit.py /srv/salt/_modules
+
 salt \* saltutil.sync_returners
 salt \* saltutil.sync_modules
 
